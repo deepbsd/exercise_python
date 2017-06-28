43c6ec0148a07a51cf954ba0e3121327919c8de4
David S. Jackson <deepbsd@yahoo.com>
2017-06-28 19:36:22 -0400

    just exercising...

--- word-count/word_count.py
+++ word-count/word_count.py
@@ -1,13 +1,12 @@
+"""Counts words and returns a dict with a count of their respective occurences"""
 def word_count(s):
+    import re
+    s = s.lower()  # lowercase all
+    s = re.sub('(\w),(\w)', "\\1 \\2",s)   # inserts a space between two words joined by a comma
+    s = re.sub(r'[^\w\s]', '',s)     # remove everything that isn't a word or space
+    s = re.sub(r'[\n\t_]|[\s]{2,20}', ' ',s)  # replaces tabs, newlines, and multiple spaces
 
-    '''Note: values must be in the order in which they appear! '''
+    outdict = { i:list(s.split(' ')).count(i) for i in list(set(s.split(' '))) }
 
-    values = [s.count(w) for w in set(s.split(' '))]
-    outdict = dict.fromkeys(set(s.split(' ')) )
 
-    index = 0
-    for k in outdict.keys():
-        outdict[k] = values[index]
-        index += 1
-
-    print(outdict)
+    return outdict
