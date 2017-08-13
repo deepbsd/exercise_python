2d9e4f2fe0af1786b83a3172646eeb1bdd515ef4
David S. Jackson <deepbsd@yahoo.com>
2017-08-13 11:39:27 -0400

    figured out how to use the maketrans/translate function in python

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,18 +1,12 @@
-def rotate(str, n):
-    output = ''
-    alpha = 'abcdefghijklmnopqrstuvwxyz'
-    alpha1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    for c in str:
-        if c not in alpha and c not in alpha1:
-            output += c
-            continue
-        if c.isupper():
-            coll = alpha1
-        else:
-            coll = alpha
-        try:
-            output += coll[coll.index(c)+n]
-        except IndexError:
-            output += coll[coll.index(c)-(26-n)]
-
-    return output
+def rotate(str, k):
+    # figured out how to use the maketrans/translate function...
+    from_string = 'abcdefghijklmnopqrstuvwxyz'
+    # create a target translation table
+    to_string = from_string[k:] + from_string[:k]
+    # add the upper case letters to both source and target
+    from_string += from_string.upper()
+    to_string += to_string.upper()
+    # here's the table for the translate function
+    table = str.maketrans(from_string, to_string)
+    # and finally, return the translation with the table as the argument
+    return str.translate(table)
