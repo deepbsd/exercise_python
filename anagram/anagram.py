42e7b402b275dc77883d538f4847b636159a6839
David S. Jackson <deepbsd@yahoo.com>
2017-08-13 15:56:56 -0400

    added anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,13 @@
-def detect_anagrams():
-    pass
+def detect_anagrams(word, array):
+    anagrams = []
+    for wd in array:
+        if len(word) != len(wd):
+            continue
+        if word.lower() == wd.lower():
+            continue
+        else:
+            if sorted(list(word.lower())) == sorted(list(wd.lower())):
+                anagrams.append(wd)
+            else:
+                continue
+    return anagrams
