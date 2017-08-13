31e5192657cf1d73d1715eac0b400a72e4cfcf39
David S. Jackson <deepbsd@yahoo.com>
2017-08-13 00:57:33 -0400

    added rotational-cipher

--- rotational-cipher/rotational_cipher.py
+++ rotational-cipher/rotational_cipher.py
@@ -1,2 +1,18 @@
-def rotate():
-    pass
+def rotate(str, n):
+    output = ''
+    alpha = 'abcdefghijklmnopqrstuvwxyz'
+    alpha1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    for c in str:
+        if c not in alpha and c not in alpha1:
+            output += c
+            continue
+        if c.isupper():
+            coll = alpha1
+        else:
+            coll = alpha
+        try:
+            output += coll[coll.index(c)+n]
+        except IndexError:
+            output += coll[coll.index(c)-(26-n)]
+
+    return output
