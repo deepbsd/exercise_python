ea4e5faca63fa6a9a49b45b7420af6d5c019ec3b
David S. Jackson <deepbsd@yahoo.com>
2017-08-14 11:44:21 -0400

    changed to a one liner

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,13 +1,2 @@
 def detect_anagrams(word, array):
-    anagrams = []
-    for wd in array:
-        if len(word) != len(wd):
-            continue
-        if word.lower() == wd.lower():
-            continue
-        else:
-            if sorted(list(word.lower())) == sorted(list(wd.lower())):
-                anagrams.append(wd)
-            else:
-                continue
-    return anagrams
+    return [ wd for wd in array if word.lower() != wd.lower() and sorted(word.lower()) == sorted(wd.lower()) ] 
