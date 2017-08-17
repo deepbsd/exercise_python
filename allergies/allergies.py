d4155b2ecec1c4b691d69dfb0d7a89864df15aa6
David S. Jackson <deepbsd@yahoo.com>
2017-08-16 22:15:31 -0400

    added allergies

--- allergies/allergies.py
+++ allergies/allergies.py
@@ -1,3 +1,10 @@
 class Allergies(object):
-    def __init__(self):
-        pass
+
+        
+    def __init__(self, score):
+        self.allergies = ['eggs', 'peanuts', 'shellfish', 'strawberries', 'tomatoes', 'chocolate', 'pollen', 'cats']
+        self.lst = [ allergy for i, allergy in enumerate(self.allergies) if 0 < (score & 1 << i)]
+
+
+    def is_allergic_to(self, allergy):
+     	return allergy in self.lst
