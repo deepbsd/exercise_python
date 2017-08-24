45c42bf998d0c5dc9dfdbbb61b570e91c895e7b6
David S. Jackson <deepbsd@yahoo.com>
2017-08-24 11:13:02 -0400

    robot-simulator runs passes now

--- robot-simulator/robot_simulator.py
+++ robot-simulator/robot_simulator.py
@@ -1,3 +1,56 @@
+NORTH="NORTH"
+SOUTH="SOUTH"
+EAST="EAST"
+WEST="WEST"
+
+direction = [NORTH, EAST, SOUTH, WEST]
+
 class Robot(object):
-    def __init__(self):
-        pass
+
+    def __init__(self, bearing=NORTH, EW=0, NS=0):
+        self.bearing = bearing
+        self.EW = EW
+        self.NS = NS
+        self.coordinates = (self.EW, self.NS)
+
+    def turn_right(self):
+        dir_index = direction.index(self.bearing)
+        if (dir_index == 3): 
+            dir_index = 0
+            self.bearing = direction[dir_index]
+        else: 
+            self.bearing = direction[dir_index+1]
+
+    def turn_left(self):
+        dir_index = direction.index(self.bearing)
+        if (dir_index == 0):
+            dir_index = 3
+            self.bearing = direction[dir_index]
+        else:
+            self.bearing = direction[dir_index-1]
+
+    def advance(self):
+        if self.bearing == NORTH:
+            self.NS += 1
+        elif self.bearing == EAST:
+            self.EW += 1
+        elif self.bearing == SOUTH:
+            self.NS -= 1
+        elif self.bearing == WEST:
+            self.EW -= 1
+        self.coordinates = (self.EW, self.NS)
+
+    def simulate(self, commands):
+        for c in commands:
+            if c == 'L':
+                self.turn_left()
+                continue
+            elif c == 'R':
+                self.turn_right()
+                continue
+            elif c == 'A':
+                self.advance()
+                continue
+            else:
+                break
+
