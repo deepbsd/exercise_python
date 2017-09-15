a452a398b551a2f7c214ecbe982da0adfde391ff
David S. Jackson <deepbsd@yahoo.com>
2017-09-15 00:59:04 -0400

    added grains

--- grains/grains.py
+++ grains/grains.py
@@ -1,6 +1,12 @@
-def on_square():
-    pass
+def on_square(num):
+    if num <= 64 and num > 0:
+        return 2**(num-1)
+    else:
+        raise ValueError
 
 
-def total_after():
-    pass
+def total_after(num):
+    if num <= 64 and num > 0:
+        return sum([2**(n-1) for n in range(1,num+1)])
+    else:
+        raise ValueError
