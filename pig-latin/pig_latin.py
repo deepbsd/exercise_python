1451682bf064368336585b04188944a7071ec077
David S. Jackson <deepbsd@yahoo.com>
2017-09-23 20:15:58 -0400

    adding simple-cipher, had problem with pig-latin

---
+++ pig-latin/pig_latin.py
@@ -0,0 +1,18 @@
+
+def translate(word):
+    if ' ' in word:
+        word = word.split(' ')
+        string = ''
+        for w in word:
+            string += translate(w) + ' '
+        return string.rstrip()
+    if word[0] in 'aeiou':
+        return word + 'ay'
+    elif word[0:3] in ['squ', 'thr', 'sch']:
+        return word[3:] + word[0:3] + 'ay'
+    elif word[0:2] in ['qu', 'ch', 'sh', 'sk', 'th']:
+        return word[2:] + word[0:2] + 'ay'
+    elif word[0:2] in ['yt', 'xr']:
+        return word + 'ay'
+    else:
+        return word[1:] + word[0] + 'ay'
