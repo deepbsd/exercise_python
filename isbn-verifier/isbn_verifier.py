5cc217ca90fcffa2f138a0b3b4e9321a488dd0af
David S. Jackson <deepbsd@yahoo.com>
2018-07-23 23:10:58 -0400

    submitted isbn-verifier

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,45 +1,17 @@
 def verify(isbn):
-    if isbn == '': 
-        return False
     string = isbn.replace('-', '')
-    if len(string) != 10:
+    if string == '' or len(string) != 10:
         return False
     num_arr = list(string)
+
     if num_arr[-1] == 'X':
         num_arr[-1] = '10'
-    print("num_arr: "+str(num_arr))
     for val in num_arr:
         try:
-            if int(val) not in range(0,10):
+            if int(val) not in range(0,11):
                 return False
-                #print("val: "+val," Array: ",num_arr)
         except ValueError:
-            #print("ValueError on Array: {} Value: {}".format(num_arr, val))
             return False
 
-    return sum([int(num_arr[n]*(10-n)) for n in range(0,9+1)]) % 11 == 0
+    return sum([int(num_arr[n])*(10-n) for n in range(0,9+1)]) % 11 == 0
 
-
-
-if __name__ == "__main__":
-    nums = [
-        '3-598-21508-8',
-        '3-598-21508-9',
-        '3-598-21507-X',
-        '3-598-21507-A',
-        '3-598-P1581-X',
-        '3-598-2X507-9',
-        '3598215088',
-        '359821507X',
-        '359821507',
-        '3598215078X',
-        '3-598-21507',
-        '3-598-21507-XX',
-        '3-598-21515-X',
-        '',
-        '134456729' ]
-    for num in nums:
-        print("Num: {} Val: {}".format(num, verify(num)))
-    #num = "3-598-21508-8"  # True example
-    #num = "3-598-21508-9"   # False example
-    #print(verify(num))
