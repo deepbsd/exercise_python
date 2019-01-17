5db3cda9086ba4f8887b130a4f02cc7eda58cc2d
David S. Jackson <deepbsd@yahoo.com>
2019-01-17 14:56:13 -0500

    got it working, now starting to refactor it

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,22 +1,29 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        self.row_of_strings = matrix_strings.split("\n")
-        self.rows = [ elem.split(" ") for elem in rows_of_strings ]
-        for arr in self.rows: 
-            for index, char in enumerate(arr):
-                arr[index] = int(char)
-        self.cols = []
-        for row in self.rows:
-            col = []
-            for val in row:
-                col.append(val)
-            self.cols.append(col)
+        #self.rows_of_strings = matrix_string.split("\n")
+        #self.rows = [ elem.split(" ") for elem in self.rows_of_strings ]
+        #for arr in self.rows: 
+        #    for index, char in enumerate(arr):
+        #        arr[index] = int(char)
+        #self.cols = []
+        #for col in self.rows[0]:
+        #    self.cols.append([])
+
+        #for col_num in range(len(self.cols)):
+        #    for arr in self.rows:
+        #        self.cols[col_num].append(arr[col_num])
+
+
+
+        self.rows = [[element.split(" ") for element in string_element] for string_element in matrix_string.split("\n")]
+        self.cols = [ list(tuple) for tuple in zip(*self.rows) ]
+
 
     def row(self, index):
-        pass
+        return self.rows[index-1]
 
     def column(self, index):
-        pass
+        return self.cols[index-1]
 
 
 
