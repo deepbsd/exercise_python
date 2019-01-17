271b8cf7b45a8701d605b259bf4c2007b1d278ca
David S. Jackson <deepbsd@yahoo.com>
2019-01-17 15:32:35 -0500

    got it working

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,6 @@
 def raindrops(number):
-    pass
+    mesg = ""
+    if number % 3 == 0: mesg += 'Pling'
+    if number % 5 == 0: mesg += 'Plang'
+    if number % 7 == 0: mesg += 'Plong'
+    return "{}".format(mesg or str(number))
