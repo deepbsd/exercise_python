aa355734095b441ab8487472ec50b424dee10fd9
David S. Jackson <deepbsd@yahoo.com>
2019-01-17 15:09:33 -0500

    okay here we go.

--- matrix/matrix.py
+++ matrix/matrix.py
@@ -1,22 +1,7 @@
 class Matrix(object):
     def __init__(self, matrix_string):
-        #self.rows_of_strings = matrix_string.split("\n")
-        #self.rows = [ elem.split(" ") for elem in self.rows_of_strings ]
-        #for arr in self.rows: 
-        #    for index, char in enumerate(arr):
-        #        arr[index] = int(char)
-        #self.cols = []
-        #for col in self.rows[0]:
-        #    self.cols.append([])
-
-        #for col_num in range(len(self.cols)):
-        #    for arr in self.rows:
-        #        self.cols[col_num].append(arr[col_num])
-
-
-
-        self.rows = [[element.split(" ") for element in string_element] for string_element in matrix_string.split("\n")]
-        self.cols = [ list(tuple) for tuple in zip(*self.rows) ]
+        self.rows = [[int(num_string) for num_string in row.split(" ")] for row in matrix_string.split("\n")]
+        self.cols = [ list(tup) for tup in zip(*self.rows) ]
 
 
     def row(self, index):
