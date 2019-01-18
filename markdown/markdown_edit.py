e46557ae375567a93a905434b4b3422d3bfb9e6c
David S. Jackson <deepbsd@yahoo.com>
2019-01-18 10:23:32 -0500

    getting closer...

--- markdown/markdown_edit.py
+++ markdown/markdown_edit.py
@@ -11,18 +11,60 @@ def parse_markdown(markdown):
 
     def return_h2(input):
         return "<h2>{}</h2>".format(input)
+    
+    def return_h6(input):
+        return "<h6>{}</h6>".format(input)
 
-    def return_italics(input):
+    def return_italic(input):
         return "<em>{}</em>".format(input)
 
     def return_bold(input):
         return "<strong>{}</strong>".format(input)
 
-    def return_ol(input):
-        return "<ol>{}</ol>".format(input)
-
-    def return_ul(input):
-        return "<ul>{}</ul>".format(input)
+    def return_open_ul(input):
+        return "<ul>{}".format(input)
+    
+    def return_close(input):
+        return "{}</ul>".format(input)
 
     def return_li(input):
         return "<li>{}</li>".format(input)
+    
+    lines = markdown.split("\n")
+
+    result = ""
+
+    for line in lines:
+        h1_match = re.match('# (.*)',line)
+        h2_match = re.match('## (.*)',line)
+        h6_match = re.match('###### (.*)',line)
+        bold_match = re.match('(.*)__(.*)__(.*)',line)
+        italic_match = re.match('(.*)_(.*)_(.*)',line)
+        li_match = re.match(r'\* (.*)',line)
+        par_match = re.match('<h|<ul|<p|<li',line)
+
+        if h1_match:
+            line = return_h1(h1_match.group(1))
+
+        if h2_match:
+            line = return_h2(h2_match.group(1))
+
+        if h6_match:
+            line = return_h6(h6_match.group(1))
+
+        if bold_match:
+            line = "{}{}{}".format(bold_match.group(1),return_bold(bold_match.group(2)),bold_match.group(3))
+
+        if italic_match:
+            line = "{}{}{}".format(italic_match.group(1),return_italic(italic_match.group(2)),italic_match.group(3))
+
+        if  re.match(r'\* (.*)', line):
+            line = "{}".format(return_li(line))
+
+        if not par_match:
+            line = "{}".format(return_paragraph(line))
+
+        result += line
+
+    return result
+
--- markdown/markdown_test.py
+++ markdown/markdown_test.py
@@ -1,55 +1,67 @@
 import unittest
-from markdown import parse_markdown
+#from markdown import parse_markdown
+from markdown_edit import parse_markdown
 
 
 # Tests adapted from `problem-specifications//canonical-data.json` @ v1.3.0
 
 class MarkdownTest(unittest.TestCase):
 
+    #@unittest.skip("skipping test...")
     def test_paragraph(self):
         self.assertEqual(parse_markdown('This will be a paragraph'),
                          '<p>This will be a paragraph</p>')
 
+    #@unittest.skip("skipping test...")
     def test_italics(self):
         self.assertEqual(parse_markdown('_This will be italic_'),
                          '<p><em>This will be italic</em></p>')
 
+    #@unittest.skip("skipping test...")
     def test_bold(self):
         self.assertEqual(parse_markdown('__This will be bold__'),
                          '<p><strong>This will be bold</strong></p>')
 
+    @unittest.skip("skipping test...")
     def test_mixed_normal_italics_and_bold(self):
         self.assertEqual(parse_markdown('This will _be_ __mixed__'),
                          '<p>This will <em>be</em> <strong>mixed</strong></p>')
 
+    @unittest.skip("skipping test...")
     def test_h1(self):
         self.assertEqual(parse_markdown('# This will be an h1'),
                          '<h1>This will be an h1</h1>')
 
+    @unittest.skip("skipping test...")
     def test_h2(self):
         self.assertEqual(parse_markdown('## This will be an h2'),
                          '<h2>This will be an h2</h2>')
 
+    @unittest.skip("skipping test...")
     def test_h6(self):
         self.assertEqual(parse_markdown(
             '###### This will be an h6'), '<h6>This will be an h6</h6>')
 
+    @unittest.skip("skipping test...")
     def test_unordered_lists(self):
         self.assertEqual(parse_markdown('* Item 1\n* Item 2'),
                          '<ul><li>Item 1</li>'
                          '<li>Item 2</li></ul>')
 
+    @unittest.skip("skipping test...")
     def test_little_bit_of_everything(self):
         self.assertEqual(parse_markdown(
             '# Header!\n* __Bold Item__\n* _Italic Item_'),
             '<h1>Header!</h1><ul><li><strong>Bold Item</strong></li>'
             '<li><em>Italic Item</em></li></ul>')
 
+    @unittest.skip("skipping test...")
     def test_symbols_in_the_header_text_that_should_not_be_interpreted(self):
         self.assertEqual(
             parse_markdown('# This is a header with # and * in the text'),
             '<h1>This is a header with # and * in the text</h1>')
 
+    @unittest.skip("skipping test...")
     def test_symbols_in_the_list_item_text_that_should_not_be_interpreted(
             self):
         self.assertEqual(
@@ -58,6 +70,7 @@ class MarkdownTest(unittest.TestCase):
             '<ul><li>Item 1 with a # in the text</li>'
             '<li>Item 2 with * in the text</li></ul>')
 
+    @unittest.skip("skipping test...")
     def test_symbols_in_the_paragraph_text_that_should_not_be_interpreted(
             self):
         self.assertEqual(
