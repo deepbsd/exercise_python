40a6a3cbda6a53785ea8a9f73eed515cc67d372c
David S. Jackson <deepbsd@yahoo.com>
2019-01-22 08:52:45 -0500

    starting on clock, some of it is working

--- clock/clock.py
+++ clock/clock.py
@@ -1,15 +1,17 @@
 class Clock(object):
     def __init__(self, hour, minute):
-        pass
+        hour += minute // 60
+        self.hour = hour % 24
+        self.minute = minute % 60
 
     def __repr__(self):
-        pass
+        return "{:02}:{:02}".format(self.hour, self.minute)
 
     def __eq__(self, other):
         pass
 
     def __add__(self, minutes):
-        pass
+        return Clock(self.hour, self.minutes + minutes)
 
     def __sub__(self, minutes):
-        pass
+        return Clock(self.hour, self.minutes - minutes)
