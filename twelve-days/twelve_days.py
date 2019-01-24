bcce8d980664bdeb4ec0365e0c88c352504bac87
David S. Jackson <deepbsd@yahoo.com>
2019-01-24 14:42:03 -0500

    finally got it perfect for today!  Twelve days is passing and takes up less space without the dict.  Replaced the dict with just a join statement--but the dict was great for beginning the conceptualization of the solution.

--- twelve-days/twelve_days.py
+++ twelve-days/twelve_days.py
@@ -18,25 +18,15 @@ def recite(start_verse, end_verse):
             "twelve Drummers Drumming",
             ]
 
-    dict = { "first": "".join(gifts[0]).replace("and ","",1),
-            "second": ", ".join(gifts[1::-1]),
-            "third": ", ".join(gifts[2::-1]),
-            "fourth": ", ".join(gifts[3::-1]),
-            "fifth": ", ".join(gifts[4::-1]),
-            "sixth": ", ".join(gifts[5::-1]),
-            "seventh": ", ".join(gifts[6::-1]),
-            "eighth": ", ".join(gifts[7::-1]),
-            "ninth": ", ".join(gifts[8::-1]),
-            "tenth": ", ".join(gifts[9::-1]),
-            "eleventh": ", ".join(gifts[10::-1]),
-            "twelfth": ", ".join(gifts[11::-1]),
-            }
     result = []
 
     for daynum in range(start_verse-1, end_verse):
         day = days[daynum]
         refrain = "On the {} day of Christmas my true love gave to me: ".format(day)
-        verse = refrain + "".join(dict[day]) + "."
+        if day == "first": 
+            verse = refrain + "".join(gifts[daynum::-1]).replace("and ","",1) + "."
+        else: 
+            verse = refrain + ", ".join(gifts[daynum::-1]) + "."
         result.append(verse)
 
     return result
