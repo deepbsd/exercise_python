d00a25a1535bfc7fb1ef0659c3d00260e4bec37f
David S. Jackson <deepbsd@yahoo.com>
2019-01-25 19:48:47 -0500

    getting closer I hope???

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -10,8 +10,6 @@ def tally(tournament_results):
             'Courageous Californians': {'MP': 0, 'W': 0, 'D': 0, 'L': 0, 'P': 0},
             }
 
-    table = header
-
     output = []
 
     lines = tournament_results.split("\n")
@@ -30,24 +28,21 @@ def tally(tournament_results):
             teams[team1]['MP'] += 1; teams[team1]['D'] += 1; teams[team1]['P'] += 1
             teams[team2]['MP'] += 1; teams[team2]['D'] += 1; teams[team2]['P'] += 1
 
-    #for team, val in teams.items():
-    #    for k,v in val.items():
-    #        if k == "P": print(team,' points: ',v)
 
     for key, team in teams.items():
-        if team['MP']: table += "{:<30s} | {!s:>2s} | {!s:>2s} | {!s:>2s} | {!s:>2s} | {!s:>2s}\n".format(key, team['MP'], team['W'], team['D'], team['L'], team['P'])
-
         if team['MP']: output.append("{:<30s} | {!s:>2s} | {!s:>2s} | {!s:>2s} | {!s:>2s} | {!s:>2s}\n".format(key, team['MP'], team['W'], team['D'], team['L'], team['P']))
         
 
 
-    output[-1] = output[-1].rstrip("\n")
     for line in output:
-        print(line)
+        line = line[::-1]
+    output = sorted(output)
+    for line in output:
+        line = line[::-1]
     
-    table = table.rstrip("\n")
-    #return table
-    return table
+        
+    
+    return header+"".join(output).rstrip("\n")
 
 
 
