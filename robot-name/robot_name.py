a8fdb5a9fd5f07fb11d3bd76c55bf2209e4c95ff
David S. Jackson <deepbsd@yahoo.com>
2019-01-28 22:19:10 -0500

    submitted robot_name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,22 +1,15 @@
 from random import choice, randint
 from string import ascii_uppercase
 import random
+from datetime import datetime
 
 class Robot(object):
     def __init__(self):
         self.name = self._create_name()
 
     def _create_name(self):
+        random.seed(datetime.now())
         return ''.join(choice(ascii_uppercase) for i in range(2))+str(randint(100,999))
 
     def reset(self):
         self.name = self._create_name()
-
-
-if __name__ == "__main__":
-    random.seed('totally random')
-    robby = Robot()
-    print(robby.name)
-    random.seed('totally random')
-    robby.reset()
-    print(robby.name)
