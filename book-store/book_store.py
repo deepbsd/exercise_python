61f216e7fc9396b0259e22bf4e1c1b3764821cc7
David S. Jackson <deepbsd@yahoo.com>
2019-02-01 11:59:58 -0500

    starting to think about a different approach here...

--- book-store/book_store.py
+++ book-store/book_store.py
@@ -12,9 +12,14 @@ def calculate_total(books):
         # this will be total cost before any discount
         total_cost += price
 
+    # compute discount for entire shebang without grouping
     for booknum, book in enumerate(purchase.values()):
         if purchase[booknum+1]: distinct_books += 1
         discount_pct = discounts[distinct_books-1]
+
+    # Should we use more than one group?
+
+
 
     total_discount1 = total_cost*discount_pct
     total_discount2 = 0.0
@@ -26,13 +31,19 @@ def calculate_total(books):
     total_cost = int((total_cost - total_discount)*100)
     return total_cost
 
+def grouper(purchases_dict):
+    # break purchases into as many groups as possible
+    # each group is sent to discounter() to get total discount
+    # return False if no groups are possible
+    pass
 
-
-
-
+def discounter(group, discounts):
+    # this takes a group as argument and returns it as a dict with 
+    # its discount as key
+    pass
 
 
 if __name__ == "__main__":
-    print(calculate_total([1,2,3,4,5]))
+    print(calculate_total([1,1,2,2,3,3,4,5]))
 
 
