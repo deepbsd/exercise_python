5ae417c9d8b4aa81b8c06eea42671b5df2d1f027
David S. Jackson <deepbsd@yahoo.com>
2019-02-04 12:41:53 -0500

    removed extra import from robot-name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,4 +1,3 @@
-from random import choice, randint
 from string import ascii_uppercase
 import random
 from datetime import datetime
@@ -9,7 +8,7 @@ class Robot(object):
 
     def _create_name(self):
         random.seed(datetime.now())
-        return ''.join(choice(ascii_uppercase) for i in range(2))+str(randint(100,999))
+        return ''.join(random.choice(ascii_uppercase) for i in range(2))+str(random.randint(100,999))
 
     def reset(self):
         self.name = self._create_name()
